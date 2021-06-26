13e9e575ed007902bcc695b8af7f835ccd68fbf7
buuoltwo <bullnick20@gmail.com>
2021-06-27 02:38:06 +0800

    feat: 10.泛型能干啥?

--- TYPESCRIPT.LAGOU/section-one/Unit10/10.ts
+++ TYPESCRIPT.LAGOU/section-one/Unit10/10.ts
@@ -3,7 +3,7 @@
  * @Author       : zhangming
  * @Date         : 2021-06-26 20:44:53
  * @LastEditors  : zhangming
- * @LastEditTime : 2021-06-27 00:13:43
+ * @LastEditTime : 2021-06-27 02:36:12
  */
 
 {
@@ -62,5 +62,72 @@ const getNumMemory = numMemory.get() // 类型是 number
 numMemory.set(2) // 只能写入 number 类型
 const strMemory = new Memory('') // 缺省 <string>
 const getStrMemory = strMemory.get() // 类型是 string
-strMemory.set('string') // 只能写入 string 类型
 // strMemory.set(+'string') // 只能写入 string 类型
+strMemory.set('string') // 只能写入 string 类型
+
+// 泛型类型
+// ->type interface
+
+//在 TypeScript 中，类型本身就可以被定义为拥有不明确的类型参数的泛型，并且可以接收明确类型作为入参，从而衍生出更具体的类型
+const reflectFn0: <P>(param: P) => P = reflect
+// 这里我们为变量 reflectFn 显式添加了泛型类型注解，并将 reflect 函数作为值赋给了它。
+
+type ReflectFuncton = <P>(param: P) => P
+const reflectFn2: ReflectFuncton = reflect
+
+interface IReflectFuncton {
+  <P>(param: P): P
+}
+const reflectFn3: IReflectFuncton = reflect
+
+// 具象化泛型
+type GenericReflectFunction<P> = (param: P) => P
+interface IGenericReflectFunction<P> {
+  (param: P): P
+}
+const reflectFn4: GenericReflectFunction<string> = reflect // 具象化泛型
+const reflectFn5: IGenericReflectFunction<number> = reflect // 具象化泛型
+const reflectFn3Return = reflectFn4('string') // 入参和返回值都必须是 string 类型
+const reflectFn4Return = reflectFn5(1) //  入参和返回值都必须是 number 类型
+
+// 在泛型定义中，我们甚至可以使用一些类型操作符进行运算表达，使得泛型可以根据入参的类型衍生出各异的类型
+type StringOrNumberArray<E> = E extends string | number ? E[] : E
+
+// >
+type StringArray = StringOrNumberArray<string> // 类型是 string[]
+type NumberArray = StringOrNumberArray<number> // 类型是 number[]
+type NeverGot = StringOrNumberArray<boolean> // 类型是 boolean
+
+// >
+type BooleanOrString = string | boolean
+type WhatIsThis = StringOrNumberArray<BooleanOrString> // 好像应该是 string | boolean ?
+type BooleanOrStringGot = BooleanOrString extends string | number
+  ? BooleanOrString[]
+  : BooleanOrString //  string | boolean
+
+// 能接受入参的泛型类型和函数一样，都可以对入参类型进行计算并返回新的类型，像是在做类型运算。
+
+//定义一个接口类型泛型ReduxModel;
+//传入一个具体的 State 类型具象化 ReduxModel
+
+interface ReduxModel<State> {
+  state: State
+  reducers: {
+    [action: string]: (state: State, action: any) => State
+  }
+}
+
+type ModelInterface = { id: number; name: string }
+const model: ReduxModel<ModelInterface> = {
+  state: { id: 4399, name: '大型在线学习网站' },
+  reducers: {
+    setIds: (state, action: { payload: number }) => ({
+      ...state,
+      id: action.payload,
+    }),
+    setName: (state, action: { payload: string }) => ({
+      ...state,
+      name: action.payload, // ok must be string
+    }),
+  },
+}
